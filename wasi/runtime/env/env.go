f34d44e74a24928bc341c9a575734f76024dac81
James Lawrence <jljatone@gmail.com>
2022-07-17 10:18:06 -0400

    make function parameter names match

--- wasi/runtime/env/env.go
+++ wasi/runtime/env/env.go
@@ -1,8 +1,6 @@
 package env
 
-// Boolean retrieve a boolean flag from the environment, checks each key in order
-// first to parse successfully is returned.
-func Boolean(fallback bool, values ...string) bool
+func Boolean(fallback bool, keys ...string) bool
 
 func Boolean2(b bool) bool
 
